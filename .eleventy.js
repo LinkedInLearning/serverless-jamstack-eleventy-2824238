1309c051644391a8ccf9476c5572341b5039defa
harol <herp05@hotmail.es>
2021-05-11 20:08:56 -0400

    added eleventy.js and copied images folder

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+ module.exports = function(eleventyConfig){
+
+    eleventyConfig.addPassthroughCopy('./_site/images');
+
+    return {
+       dir: {
+          input: '_site',
+          output: 'dist'
+       }
+    }
+ }
